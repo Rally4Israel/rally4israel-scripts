a9e2d3b85f5c013d6b47c64b6b0f622686f61783
Ari Abramowitz <ari.abramowitz1@gmail.com>
2024-10-20 12:58:19 +0300

    test: Add getColIdx, appendRow to MockSheetAPI

--- tests/mockers.js
+++ tests/mockers.js
@@ -10,6 +10,23 @@ class MockSheetAPI {
         return this.sheetData.slice(1)
     }
 
+    appendRow(rowData) {
+        this.sheetData.push(rowData)
+    }
+
+    getColumnIndexMap() {
+        let columnIndexMap = {}
+        let header = this.sheetData[0];
+        for (let i = 0; i < header.length; i++) {
+            columnIndexMap[header[i]] = i;
+        }
+        return columnIndexMap
+    }
+
+    getColIdx(columnName) {
+        return this.getColumnIndexMap()[columnName]
+    }
+
 }
 
 module.exports = { MockSheetAPI };
--- tests/testMockers/mockSheetAPI.test.js
+++ tests/testMockers/mockSheetAPI.test.js
@@ -12,3 +12,22 @@ test('getAllData skips header row', () => {
     expect(data.length).toStrictEqual(1)
     expect(data[0][0]).toStrictEqual("data1")
 })
+
+test('getCoIdx returns index for column name', () => {
+    const sheetAPI = new MockSheetAPI(initialData = [["col1", "col2"]])
+    const col1Idx = sheetAPI.getColIdx('col1')
+    const col2Idx = sheetAPI.getColIdx('col2')
+    expect(col1Idx).toBe(0)
+    expect(col2Idx).toBe(1)
+})
+
+test('appendRow adds row to sheetData', () => {
+    const sheetAPI = new MockSheetAPI(initialData = [["col a", "col b"]])
+    sheetAPI.appendRow(["val 1a", "val 1b"])
+    expect(sheetAPI.getAllData().length).toBe(1)
+    sheetAPI.appendRow(["val 2a", "val 2b"])
+    expect(sheetAPI.getAllData().length).toBe(2)
+    let data = sheetAPI.getAllData()
+    expect(data[0][0]).toStrictEqual("val 1a")
+    expect(data[1][0]).toStrictEqual("val 2a")
+})
