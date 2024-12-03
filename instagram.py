6cb38ca2c39a2396a6d53c408d96a3902a2da51b
Ari Abramowitz <ari.abramowitz1@gmail.com>
2024-12-03 21:17:43 +0200

    feat: Add collaborators to insta posts

--- instagram.py
+++ instagram.py
@@ -13,10 +13,16 @@ def main():
 
     cl = Client()
     cl.login(INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD)
+    # collaborators_usernames = ["elizabeth.rand.311", "herutnyc"]
+    collaborators_usernames = ["ari.abramowitz1"]
+    collaborator_ids = [
+        cl.user_id_from_username(username) for username in collaborators_usernames
+    ]
 
     media = cl.album_upload(
         paths=[IMAGE_PATH, IMAGE_PATH],
         caption="testing something...",
+        extra_data={"invite_coauthor_user_ids": collaborator_ids},
     )
     print(media)
 
