7d4f2ac794a2f1a1a278dd7dc27da7ce20933cba
Ari Abramowitz <ari.abramowitz1@gmail.com>
2024-12-03 23:23:04 +0200

    feat: Allow insta login by sessionid

--- instagram.py
+++ instagram.py
@@ -5,23 +5,27 @@ load_dotenv()
 
 INSTAGRAM_USERNAME = os.getenv("INSTAGRAM_USERNAME")
 INSTAGRAM_PASSWORD = os.getenv("INSTAGRAM_PASSWORD")
+INSTAGRAM_SESSION_ID = os.getenv("INSTAGRAM_SESSION_ID")
 IMAGE_PATH = "img/testing.jpg"
 
 
 def main():
     from instagrapi import Client
 
-    cl = Client()
+    client = Client()
     print(len(INSTAGRAM_USERNAME))
     print(len(INSTAGRAM_PASSWORD))
-    cl.login(INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD)
+    if INSTAGRAM_SESSION_ID:
+        client.login_by_sessionid(INSTAGRAM_SESSION_ID)
+    else:
+        client.login(INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD)
     # collaborators_usernames = ["elizabeth.rand.311", "herutnyc"]
     collaborators_usernames = ["ari.abramowitz1"]
     collaborator_ids = [
-        cl.user_id_from_username(username) for username in collaborators_usernames
+        client.user_id_from_username(username) for username in collaborators_usernames
     ]
 
-    media = cl.album_upload(
+    media = client.album_upload(
         paths=[IMAGE_PATH, IMAGE_PATH],
         caption="testing something...",
         extra_data={"invite_coauthor_user_ids": collaborator_ids},
