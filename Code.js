9558f24acbdac28b9dd9ed84aa8e5f69efa17a58
Ari Abramowitz <ari.abramowitz1@gmail.com>
2024-12-05 22:04:14 +0200

    feat: Include rally4israelteam calendar in syncEvents

--- Code.js
+++ Code.js
@@ -1,10 +1,11 @@
 
 function syncEvents() {
   let israelChantsCalendarAPI = new GCalAPI('israelchants@gmail.com')
+  let rally4IsraelTeamCalendarAPI = new GCalAPI('rally4israelteam@gmail.com')
   let airtableEventsAPI = new AirtableAPI(secrets.AIRTABLE.URLS.EVENTS, ["GCalID"])
   let airtableUsersAPI = new AirtableAPI(secrets.AIRTABLE.URLS.USERS, ["Email"])
   let sycner = new GCalToAirtableSyncer(
-    [israelChantsCalendarAPI],
+    [israelChantsCalendarAPI, rally4IsraelTeamCalendarAPI],
     airtableEventsAPI,
     airtableUsersAPI
   )
