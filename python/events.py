39e0eb8325534d984e3505d15cc06fc62e647da5
Ari Abramowitz <ari.abramowitz1@gmail.com>
2024-12-05 22:45:44 +0200

    feat: Add airtable_record_to_event function

--- python/events.py
+++ python/events.py
@@ -1,9 +1,27 @@
 from dataclasses import dataclass
+from datetime import datetime
 
 
 @dataclass
 class Event:
-    name: str
+    title: str
     date: str
-    time: str
+    start_time: str | None
     location: str
+
+
+def airtable_record_to_event(record: dict) -> Event:
+    record_fields = record["fields"]
+    start_time_str = record_fields.get("Start Time")
+    if start_time_str:
+        start_time = datetime.strptime(start_time_str, "%I:%M%p").time()
+    else:
+        start_time = None
+    date = datetime.strptime(record_fields.get("Date"), "%Y-%m-%d").date()
+
+    return Event(
+        title=record_fields.get("Title"),
+        date=date,
+        start_time=start_time,
+        location=record_fields.get("Location"),
+    )
---
+++ python/tests/events/test_airtable_record_to_event.py
@@ -0,0 +1,52 @@
+from datetime import date, time
+from python.events import airtable_record_to_event
+
+
+def test_copies_title_and_location():
+    record = {
+        "fields": {
+            "Title": "Test Event",
+            "Location": "Somewhere",
+            "Start Time": "9:00am",
+            "Date": "2024-01-02",
+        }
+    }
+    event = airtable_record_to_event(record)
+
+    assert event.title == "Test Event"
+    assert event.location == "Somewhere"
+
+
+def test_converts_start_time_to_time_object():
+    record = {
+        "fields": {
+            "Title": "Test Event",
+            "Start Time": "9:00am",
+            "Date": "2024-01-02",
+        }
+    }
+    event = airtable_record_to_event(record)
+    assert event.start_time == time(9, 0)
+
+
+def test_converts_start_time_to_None_if_no_start_time():
+    record = {
+        "fields": {
+            "Title": "Test Event",
+            "Date": "2024-01-02",
+        }
+    }
+    event = airtable_record_to_event(record)
+    assert event.start_time is None
+
+
+def test_converts_date_to_date_object():
+    record = {
+        "fields": {
+            "Title": "Test Event",
+            "Start Time": "9:00am",
+            "Date": "2024-01-02",
+        }
+    }
+    event = airtable_record_to_event(record)
+    assert event.date == date(2024, 1, 2)
